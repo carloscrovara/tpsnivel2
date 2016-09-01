b417dfc16dded580a45096d20ee01080a1579f22
carloscrovara <carloscrovara@gmail.com>
2016-09-01 16:44:26 -0300

    versión 1.0

---
+++ TP1/js/index.js
@@ -0,0 +1,15 @@
+var numeroA = parseInt (prompt("Ingresar número"));
+var numeroB = parseInt (prompt("Ingresar número"));
+alert(numeroA + numeroB);
+ 
+var numeroC = parseInt (prompt("Ingresar número"));
+var numeroD = parseInt (prompt("Ingresar número"));
+alert(numeroC - numeroD);
+ 
+var numeroE = parseInt (prompt("Ingresar número"));
+var numeroF = parseInt (prompt("Ingresar número"));
+alert(numeroE * numeroF);
+ 
+var numeroG = parseInt (prompt("Ingresar número"));
+var numeroH = parseInt (prompt("Ingresar número"));
+alert(numeroG / numeroH);
