6bbe9e42261a348f405c8f4f8de60eb4bd832c87
carloscrovara <carloscrovara@gmail.com>
2016-09-16 17:31:38 -0300

    Versión 1.4

--- TP5/js/script.js
+++ TP5/js/script.js
@@ -4,27 +4,27 @@ function Pelicula(id,titulo) {
 	this.id = id;
 	this.titulo = titulo;
 
-    this.getId = function () {
+	this.getId = function () {
         return this.id;
     }
-    this.getTitulo = function () {
+	this.getTitulo = function () {
         return this.titulo;
     }
 
 }
 
 function agregarPelicula(peliculas) {
-         	    var continuar = 1;
-    		    var nuevaId;
-                var nuevoTitulo;
-                var nuevaPelicula;
-    		    do {
-       					nuevaId = prompt('Ingresar Id de nueva pelicula');
-                    	nuevoTitulo = prompt('Ingresar titulo de nueva pelicula');                                
-                    	nuevaPelicula = new Pelicula(nuevaId, nuevoTitulo);                            
-						peliculas.push(nuevaPelicula);
-       					continuar = parseInt(prompt('Ingrese 1 para agregar otra pelicula o 0 para salir'));
-    		    } while(continuar);
+	var continuar = 1;
+	var nuevaId;
+	var nuevoTitulo;
+	var nuevaPelicula;
+	do {
+		nuevaId = prompt('Ingresar Id de nueva pelicula');
+		nuevoTitulo = prompt('Ingresar titulo de nueva pelicula');                                
+		nuevaPelicula = new Pelicula(nuevaId, nuevoTitulo);                            
+		peliculas.push(nuevaPelicula);
+		continuar = parseInt(prompt('Ingrese 1 para agregar otra pelicula o 0 para salir'));
+	} while(continuar);
 }
 
 
